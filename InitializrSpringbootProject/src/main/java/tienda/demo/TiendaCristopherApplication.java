da62e213e2bb9b5aaa8e612307132fc94236874b
Cristopher Rodríguez <145072878+trebleprodigy@users.noreply.github.com>
2024-01-24 18:34:51 -0600

    creación de proyectos

---
+++ InitializrSpringbootProject/src/main/java/tienda/demo/TiendaCristopherApplication.java
@@ -0,0 +1,13 @@
+package tienda.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaCristopherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaCristopherApplication.class, args);
+	}
+
+}
