d10accf55e05b64c5a824b2a4e83b828fd03d8b1
Cristopher Rodríguez <145072878+trebleprodigy@users.noreply.github.com>
2024-03-08 20:32:17 -0600

    Semana8 - 8

--- InitializrSpringbootProject/src/main/java/tienda/controller/CategoriaController.java
+++ InitializrSpringbootProject/src/main/java/tienda/controller/CategoriaController.java
@@ -4,13 +4,18 @@
  */
 package tienda.controller;
 
+import tienda.domain.Categoria;
 import tienda.service.CategoriaService;
+import tienda.service.impl.FirebaseStorageServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  *
@@ -30,5 +35,40 @@ public class CategoriaController {
         model.addAttribute("categorias", categorias);
         model.addAttribute("totalCategorias", categorias.size());
         return "/categoria/listado";
+    }
+    
+    @GetMapping("nuevo")
+    public String categoriaNuevo(Categoria categoria) {
+        return "/categoria/modifica";
+    }
+    
+    @Autowired
+    private FirebaseStorageServiceImpl firebaseStorageService;
+    
+    @PostMapping("/guardar")
+    public String categoriaGuardar(Categoria categoria, 
+            @RequestParam("imagenFile") MultipartFile imagenFile) {
+        if (!imagenFile.isEmpty()) {
+            categoriaService.save(categoria);
+            categoria.setRutaImagen(firebaseStorageService.cargaImagen(
+                    imagenFile, 
+                    "categoria", 
+                    categoria.getIdeCategoria()));
+        }
+        categoriaService.save(categoria);
+        return "redirect:/categoria/listado";
+    }
+    
+    @GetMapping("/eliminar/{idCategoria}")
+    public String categoriaModificar(Categoria categoria) {
+        categoriaService.delete(categoria);
+        return "redirect:/categoria/listado";
+    }
+    
+    @GetMapping("/eliminar/{idCategoria}")
+    public String categoriaModificar(Categoria categoria, Model model) {
+        categoria = categoriaService.getCategoria(categoria);
+        model.addAttribute("categoria", categoria);
+        return "/categoria/modifica";
     }
 }
